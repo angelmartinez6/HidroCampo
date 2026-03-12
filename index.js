17d165d8c3044c5581760860b601ef607b006703
Angel <martinezovedgalindo@gmail.com>
2026-03-12 10:38:45 -0600

    Corrigiendo puerto para Railway

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ app.post('/api/cultivo', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, '0.0.0.0', () => console.log(`🚀 Servidor API corriendo en puerto ${PORT}`));
 
 // --- LÓGICA MQTT (SENSORES) ---
